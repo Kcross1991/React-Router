f586d99214aeaddf7457875094160e9a34368939
root <root@Kacy>
2023-12-18 21:22:56 -0600

    Colors present

--- React Router/src/App.jsx
+++ React Router/src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route, Link } from "react-router-dom";
-import './App.css'
-import Blue from './components/blue.jsx'
-import Red from './components/red.jsx'
+import './App.css';
+import Blue from './components/blue.jsx';
+import Red from './components/red';
+import Home from './components/Home';
+
 
 function App() {
   
@@ -11,14 +13,15 @@ function App() {
     <div id="container">
       <h1>Hello React Router!</h1>
       <div id="navbar">
-        <Link to='/'>Blue</Link>
-        <Link to='/'> Red</Link>
-        <Link to='/'>Home</Link></div>
+      <Link to='/'>Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        </div>
       <div id="main-section">
         <Routes>
-        <Route path="/blue" Blue={<h1>Blue</h1>} />
-        <Route path="/red" Red={<h1>Red</h1>} /> 
-        <Route path="/home" Home={<h1>Home</h1>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/blue" element={<Blue/>} />
+        <Route path="/red" element={<Red/>} /> 
         </Routes>
           </div>
     </div>
